0546e9b46bcbf65ce19c42b9659fd1184c3a21e2
Nonsenses <yarygi66@gmail.com>
2020-06-18 20:51:26 +0300

    slider

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $('.burger').on('click', function(){
         $('.burger, .nav_mobile').toggleClass('active')
-    })
+    });
+    $('.slider').slick({
+        dots: true,
+        arrows:false
+    });
+
 })
